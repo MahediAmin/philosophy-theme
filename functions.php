8cd5bb8b150480bf7554cd12c21971d28175da1d
mahedi <mahedi_cse@hotmail.com>
2018-08-07 20:02:41 +0600

    menu option added

--- functions.php
+++ functions.php
@@ -84,3 +84,23 @@ function philosophy_assets_list(){
 add_action("wp_enqueue_scripts", "philosophy_assets_list");
 
 
+
+/*
+ * pagination for listed pagination
+ */
+
+
+function philosophy_pagination() {
+    global $wp_query;
+    $links = paginate_links( array(
+        'current'  => max( 1, get_query_var( 'paged' ) ),
+        'total'    => $wp_query->max_num_pages,
+        'type'     => 'list',
+        'mid_size' => 3
+    ) );
+    $links = str_replace( "page-numbers", "pgn__num", $links );
+    $links = str_replace( "<ul class='pgn__num'>", "<ul>", $links );
+    $links = str_replace( "next pgn__num", "pgn__next", $links );
+    $links = str_replace( "prev pgn__num", "pgn__prev", $links );
+    echo $links;
+}
--- template-parts/common/navigation.php
+++ template-parts/common/navigation.php
@@ -1,10 +1,30 @@
-<?php wp_nav_menu(array(
+<?php //wp_nav_menu(array(
+//
+//    'theme_location' =>  'top_menu',
+//    'menu_id'        =>  'top_menu',
+//    'menu_class' => 'header__nav',
+//    'container'      =>  '',
+//
+//
+//
+//));?>
 
-    'theme_location' =>  'top_menu',
-    'menu_id'        =>  'top_menu',
+<?php
+
+
+/*
+* wp_menu query with child class
+*/
+
+?>
+
+<?php
+$philosophy_menu = wp_nav_menu(array(
+    'theme_location' => 'top_menu',
+    'menu_id' => 'top_menu',
     'menu_class' => 'header__nav',
-    'container'      =>  '',
-
-
-
-));?>
+    'echo' => false
+));
+$philosophy_menu = str_replace("menu-item-has-children", "menu-item-has-children has-children", $philosophy_menu);
+echo $philosophy_menu;
+?>
